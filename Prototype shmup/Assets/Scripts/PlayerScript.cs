741c8194a56e84a84918b18c7689b6901e770b71
JhinKiat <jhinkiat915@gmail.com>
2022-10-20 15:28:31 +0800

    start up

--- Prototype shmup/Assets/Scripts/PlayerScript.cs
+++ Prototype shmup/Assets/Scripts/PlayerScript.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float movementSpeed;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,30 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+            transform.position += transform.up * movementSpeed * Time.deltaTime;
+           
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * movementSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * movementSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * movementSpeed * Time.deltaTime;
+
+        }
+
     }
 }
