680a47d3d0d355d475276cae4cb81abd7bdd6fde
JhinKiat <jhinkiat915@gmail.com>
2022-11-02 16:37:08 +0800

    laser

--- Prototype shmup/Assets/Scripts/LaserScript.cs
+++ Prototype shmup/Assets/Scripts/LaserScript.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class LaserScript : MonoBehaviour
 {
+    [SerializeField] private float defDistanceRay = 200;
+    public Transform laserFirePoint;
+    public LineRenderer m_lineRenderer;
+    Transform m_transform;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        m_transform = GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            ShootLaser();
+            print("Activate laser weapon");
+        }
         
     }
 
     // Update is called once per frame
-    void Update()
+    void ShootLaser()
     {
-        
+        if (Physics2D.Raycast(m_transform.position, transform.up))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast
+                (laserFirePoint.position, transform.up);
+            Draw2DRay(laserFirePoint.position, _hit.point);
+        }
+        else
+        {
+            Draw2DRay(laserFirePoint.position, 
+                laserFirePoint.transform.right 
+                * defDistanceRay);
+        }
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos )
+    {
+        m_lineRenderer.SetPosition(0, startPos);
+        m_lineRenderer.SetPosition(1, endPos);
     }
 }
