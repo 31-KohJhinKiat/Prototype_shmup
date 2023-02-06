c74fe5ea95fa0a78796abf01fea4a3815300523d
JhinKiat <jhinkiat915@gmail.com>
2023-02-06 16:49:59 +0800

    Different types of Power Ups

---
+++ Prototype shmup/Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour
+{
+    public bool activateSheld;
+    public bool addGuns;
+    public bool increaseSpeed;
+    public bool increaseBombs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
